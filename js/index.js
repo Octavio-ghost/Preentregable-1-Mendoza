3a45cfdda54d6b16e9231215cd41fb26f4afcc4a
Octavio Mendoza <octaviomendoza602@gmail.com>
2024-08-11 17:39:13 -0300

    Proyecto JS

---
+++ js/index.js
@@ -0,0 +1,24 @@
+function comprar(){
+let producto= prompt("Ingresar producto")
+gorra.push(producto)
+}
+
+const gorra =[]
+
+
+
+comprar()
+
+if(producto=gorra){
+    alert('Comprado')
+}
+
+for(const producto of gorra){
+    console.log('Compraste una gorra')
+} 
+
+
+
+
+
+
